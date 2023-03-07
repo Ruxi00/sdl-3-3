742fc4f51734d86bd4b5f81c17893ce4da5c2745
Ruxi00 <125533994+Ruxi00@users.noreply.github.com>
2023-03-07 21:13:23 +0800

    Update persecoding.py

--- persecoding.py
+++ persecoding.py
@@ -8,4 +8,61 @@ total_wheels = unicycles * 1 + scooters * 2 + tricycles * 3
 
 print(total_wheels)
 
-#
+#PCTC 2023 R0 Q2 (without chat gpt help)
+
+mass1 = int(input())
+volume1 = int(input())
+mass2 = int(input())
+volume2 = int(input())
+
+density1 = mass1 / volume1
+density2 = mass2 / volume2
+
+if density1 > density2:
+  print("1")
+
+if density1 < density2:
+  print("2")
+
+#PCTC 2023 R0 Q2 (with chat gpt help)
+
+mass1 = int(input())
+volume1 = int(input())
+mass2 = int(input())
+volume2 = int(input())
+
+density1 = mass1 / volume1
+density2 = mass2 / volume2
+
+if density1 > density2:
+  print("1")
+
+elif density1 < density2:
+  print("2")
+  
+else:
+  print("same")
+  
+#PCTC 2023 R0 Q3
+
+word = input()
+double_up_word = ""
+for letter in word:
+    double_up_word += letter * 2
+print(double_up_word)
+
+#PCTC 2023 R0 Q4
+
+sentence = input()
+sentence2 = f'you said "{sentence}"?'
+print(sentence2)
+
+#PCTC 2023 R0 Q5
+
+units = input()
+
+units == "metric";
+mass = int(input())
+formatted_mass = f"{mass} kg"
+
+print(formatted_mass)
