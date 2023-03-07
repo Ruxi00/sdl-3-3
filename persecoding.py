c73ae01e442d9241d0f0892ad3b87c73c551c295
Ruxi00 <125533994+Ruxi00@users.noreply.github.com>
2023-03-07 19:58:01 +0800

    Create persecoding.py

---
+++ persecoding.py
@@ -0,0 +1,11 @@
+#PCTC 2023 R0 Q1
+
+unicycles = int(input())
+scooters = int(input())
+tricycles = int(input())
+
+total_wheels = unicycles * 1 + scooters * 2 + tricycles * 3
+
+print(total_wheels)
+
+#
